434c52edf98c8ccbfc201a660a3c1c44b8f24399
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-03-26 23:29:09 +0200

    props and mapping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,42 @@ import './style.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Data from "./data"
 
 function App() {
+  const cards = Data.map(
+    item => {
+      return (
+        <Card 
+        key={item.id}
+        /*item = {item}
+        We can use this, but then instead of props.property 
+        it will be props.item.property 
+        Note: We need to take care of the property name in the item (data file)
+        
+
+        we can also use 
+        {..item} which is a spread item that passes the data into the component 
+        using the same name of the item properties not props properties
+        */
+        img="icon.png"
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots = {item.openSpots}
+     />
+      )
+    }
+  )
   return (
     <div className="App">
      <Navbar />
      <Hero />
-     <Card />
+     <section className="cards-list">
+       {cards}
+      </section>
     </div>
   );
 }
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,19 +1,28 @@
 import React from "react";
-import imag from "../images/cathy.png"
 import star from "../images/star.png"
-
 export default function Card(props){
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return(
         <div className="card">
-            <img src={imag} className="card--image"></img>
+            {badgeText 
+            && 
+            <div className="card--badge"> {badgeText}</div>}
+            
+            <img src={require(`../images/${props.img}`)}
+             className="card--image"></img>
             <div className="card-status">
                 <img src = {star} className="card--star"></img>
-                <span >5.0</span>
-                <span className="gray">(6) . </span>
-                <span className="gray">USA</span>
+                <span >{props.rating}</span>
+                <span className="gray">({props.reviewCount}) . </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136 </span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price} </span> / person</p>
         </div>
     )
 }
